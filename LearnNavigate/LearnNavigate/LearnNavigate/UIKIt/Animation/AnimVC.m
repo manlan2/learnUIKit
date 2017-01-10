63454eb4125a3ee856c616561a79092fd1711c64
wanhongbin <wanhongbin@wanhongbins-MacBook-Pro.local>
2017-01-10 10:49:04 +0800

    动画效果

--- LearnNavigate/LearnNavigate/LearnNavigate/UIKIt/Animation/AnimVC.m
+++ LearnNavigate/LearnNavigate/LearnNavigate/UIKIt/Animation/AnimVC.m
@@ -10,7 +10,57 @@
 - (void)viewDidLoad {
 
     [super viewDidLoad];
-    [self initAnimtion];
+    [self.view setBackgroundColor:[UIColor clearColor]];
+
+    [self initAnimtion3];
+}
+
+- (void)initAnimtion3 {
+
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 100, 100, 100)];
+    // 给图片控件添加图片对象
+    [imageView setImage:[UIImage imageNamed:@"image_00000"]];
+    //图片控件添加到视图上面去
+    [self.view addSubview:imageView];
+
+    //创建一个可变数组
+    NSMutableArray *ary = [NSMutableArray new];
+    for(int i = 0; i <= 9; i ++) {
+
+        //通过for 循环,把我所有的 图片存到数组里面
+        NSString *imageName = [NSString stringWithFormat:@"image_0000%d", i];
+        NSLog(@"%@", imageName);
+
+        UIImage  *image     = [UIImage imageNamed:imageName];
+        [ary addObject:image];
+    }
+
+    // 设置图片的序列帧 图片数组
+    imageView.animationImages = ary;
+    //动画重复次数
+    imageView.animationRepeatCount = 0;
+    //动画执行时间,多长时间执行完动画
+    imageView.animationDuration    = 0.20;
+    //开始动画
+    [imageView startAnimating];
+}
+
+- (void)initAnimtion2 {
+    NSMutableArray *images = [NSMutableArray array];
+    for (NSInteger i = 0; i <= 34; i++)
+    {
+        NSString * str;
+        if(10 > i)
+        {
+            str = [NSString stringWithFormat:@"0%zd",i];
+        }
+        else {
+            str = [NSString stringWithFormat:@"%zd",i];
+        }
+
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"loading_000%@",str]];
+        [images addObject:image];
+    }
 }
 
 - (void)initAnimtion {
@@ -45,11 +95,13 @@
                 nil];
 
     self.animationImageView = [[UIImageView alloc] init];
-    [self.animationImageView initWithFrame:CGRectMake(0, 0, 131, 125)];
+    [self.animationImageView initWithFrame:CGRectMake(0, 64, 131, 125)];
     self.animationImageView.animationImages      = imagesArray;//将序列帧数组赋给UIImageView的animationImages属性
     self.animationImageView.animationDuration    = 3.f;//设置动画时间
     self.animationImageView.animationRepeatCount = 0;//设置动画次数 0 表示无限
     [self.animationImageView startAnimating];//开始播放动画
+
+    [self.view addSubview:self.animationImageView];
 
     NSTimer *myAnimatedTimer = [NSTimer scheduledTimerWithTimeInterval:0.04 target:self selector:@selector(setNextImage) userInfo:nil repeats:YES];
 }
