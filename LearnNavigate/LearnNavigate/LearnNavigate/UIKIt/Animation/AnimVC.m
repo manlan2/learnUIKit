7107e635ff5cc999f854a98818556cdc370f4053
wanhongbin <wanhongbin@wanhongbins-MacBook-Pro.local>
2017-01-11 15:58:53 +0800

    动画修改

--- LearnNavigate/LearnNavigate/LearnNavigate/UIKIt/Animation/AnimVC.m
+++ LearnNavigate/LearnNavigate/LearnNavigate/UIKIt/Animation/AnimVC.m
@@ -18,9 +18,6 @@
 }
 
 - (void)initAnimtion {
-    
-    self.imageView       = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 198, 155)];
-    //    [imageView setImage:[UIImage imageNamed:@"image_00000"]];
     self.imageView.image = [UIImage imageNamed:@"image_00000"];
     [self.view addSubview:self.imageView];
     
@@ -86,22 +83,17 @@
     [self.view addSubview:view1];
 }
 
-// 图片旋转360度
-- (UIImageView *)rotate360DegreeWithImageView:(UIImageView *)imageView{
+- (void)rotate360DegreeWithImageView:(UIImageView *)imageView{
     CABasicAnimation *animation = [ CABasicAnimation
         animationWithKeyPath: @"transform" ];
     animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
-
-    //围绕Z轴旋转，垂直与屏幕
     animation.toValue = [ NSValue valueWithCATransform3D:
 
         CATransform3DMakeRotation(M_PI, 0.0, 0.0, 1.0) ];
-    animation.duration = 0.5;
-    //旋转效果累计，先转180度，接着再旋转180度，从而实现360旋转
+    animation.duration = 1;
     animation.cumulative = YES;
     animation.repeatCount = 1000;
 
-    //在图片边缘添加一个像素的透明区域，去图片锯齿
     CGRect imageRrect = CGRectMake(0, 0,imageView.frame.size.width, imageView.frame.size.height);
     UIGraphicsBeginImageContext(imageRrect.size);
     [imageView.image drawInRect:CGRectMake(1,1,imageView.frame.size.width-2,imageView.frame.size.height-2)];
@@ -109,7 +101,6 @@
     UIGraphicsEndImageContext();
 
     [imageView.layer addAnimation:animation forKey:nil];
-    return imageView;
 }
 
 @end
